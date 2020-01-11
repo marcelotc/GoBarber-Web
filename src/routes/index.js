867a23ebbab4f5c8a369f9124e2cf133e2cef22a
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-11 17:54:19 -0300

    Estilos globais

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
-export default function () {
+export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={SignIn}></Route>
