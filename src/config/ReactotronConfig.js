0df575e5ec63c182065c4e333196cd09daf1ae46
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-11 23:50:40 -0300

    Configurando store(Redux, Saga, Reactotron)

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,9 @@
 import Reactron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-    const tron = Reactron.configure().connect();
+    const tron = Reactron.configure().use(reactotronRedux()).use(reactotronSaga()).connect();
 
     tron.clear();
 
