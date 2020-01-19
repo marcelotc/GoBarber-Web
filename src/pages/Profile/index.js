4ef94a038f0a2c75b700e79a26616bb6e7852006
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-19 15:32:08 -0300

    Página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+
+  }
+
   return (
-    <h1>Profile</h1>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo"></Input>
+        <Input name="email" type="email" placeholder="Seu endereço de email"></Input>
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Sua senha atual"></Input>
+        <Input type="password" name="password" placeholder="Nova senha"></Input>
+        <Input type="password" name="confirmPassword" placeholder="Confirmação de senha"></Input>
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
