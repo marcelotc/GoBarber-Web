8ec36959f8e87fcacfe8a12d02ef3b4882da06d9
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-20 00:38:05 -0300

    Estilização do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-
-  api.get('appointments');
-
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>8:00</strong>
+          <span>Marcelo Cortes</span>
+        </Time>
+        <Time available>
+          <strong>9:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Marcelo Cortes</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
